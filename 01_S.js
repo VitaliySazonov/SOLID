dd794e0ecbba8ba4f29de41bc8b52cecacd8e840
Vito <topvetal@gmail.com>
2020-03-26 00:33:34 +0200

    last commit

---
+++ 01_S.js
@@ -0,0 +1,71 @@
+// Single Responsibility Principle
+
+class News {
+    constructor(title, text) {
+        this.title = title;
+        this.text = text;
+        this.modified = false;
+    }
+
+    update(test) {
+        this.text = test;
+        this.modified = true;
+    }
+
+    // toHTML() {
+    //     return `
+    //     <div class="news">
+    //         <h1>${this.title}</h1>
+    //         <p>${this.text}</p>
+    //     </div>`
+    // }
+    //
+    // toJSON () {
+    //     return JSON.stringify({
+    //         title: this.title,
+    //         text: this.text,
+    //         modified: this.modified
+    //     }, null, 2);
+    // }
+}
+
+class NewsPrinter {
+    constructor(news) {
+        this.news = news;
+    }
+
+    html() {
+        return `
+        <div class="news">
+            <h1>${this.news.title}</h1>
+            <p>${this.news.text}</p>
+        </div>`
+    }
+
+    json () {
+        return JSON.stringify({
+            title: this.news.title,
+            text: this.news.text,
+            modified: this.news.modified
+        }, null, 2);
+    }
+
+    xml() {
+        return `
+            <news>
+                <title>${this.news.title}</title>
+                <test>${this.news.test}</test>
+            </news> 
+        `
+    }
+}
+
+const printer = new NewsPrinter(
+    new News('Putin', 'New const')
+);
+
+console.log(printer.html());
+console.log(printer.xml());
+console.log(printer.json());
+
+
